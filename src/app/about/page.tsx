58fbd99805329d2e6ca347d8c5f9d0ee683d0b9c
Danylo <danylo.santoro@gmail.com>
2023-12-02 20:50:40 +0100

    add about me and contect route

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,21 +8,35 @@ export default function About() {
       {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
       <p> /**</p>
       <p> * About me</p>
-      <p> * I have 5 years of еxperience in web</p>
-      <p> * development lorem ipsum dolor sit amet,</p>
-      <p> * consectetur adipiscing elit, sed do eiusmod</p>
-      <p> * tempor incididunt ut labore et dolore</p>
-      <p> * magna aliqua. Ut enim ad minim veniam,</p>
-      <p> * quis nostrud exercitation ullamco laboris</p>
-      <p> * nisi ut aliquip ex ea commodo consequat.</p>
-      <p> * Duis aute irure dolor in reprehenderit in</p>
+      <p> * Hello there! My name is Danylo Santoro,</p>
+      <p> * a seasoned frontend developer with a</p>
+      <p> * passion for crafting exceptional digital experiences.</p>
+      <p> * With a robust background spanning over a decade</p>
+      <p> * in the dynamic realm of frontend development,</p>
+      <p> * I have had the privilege of contributing my skills</p>
+      <p> * to numerous companies across the globe.</p>
       <p> *</p>
-      <p> * Duis aute irure dolor in reprehenderit in</p>
-      <p> * voluptate velit esse cillum dolore eu fugiat </p>
-      <p> * nulla pariatur. Excepteur sint occaecat </p>
-      <p> * officia deserunt mollit anim id est laborum.</p>
+      <p> * My journey has been marked by a diverse array of projects,</p>
+      <p> * each presenting unique challenges that have only fueled</p>
+      <p>
+        {" "}
+        * my enthusiasm for creating innovative and user-centric solutions.
+      </p>
+      <p> * From responsive web designs to cutting-edge user interfaces,</p>
+      <p> * I&apos;ve been at the forefront of transforming ideas into </p>
+      <p>* visually stunning and functionally seamless realities.</p>
+      <p> * </p>
+      <p> * One of the most rewarding aspects of my career has been the</p>
+      <p> * opportunity to collaborate with talented individuals.</p>
+      <p> * I find immense satisfaction in helping my colleagues</p>
+      <p> * grow and develop their skills.</p>
+      <p> * Whether through mentorship, knowledge-sharing sessions,</p>
+      <p> * or collaborative problem-solving, I believe in</p>
+      <p> * fostering a culture of continuous learning and improvement.</p>
       <p> */</p>
       <p></p>
     </div>
   );
 }
+
+//👋 Hello there! I'm [Your Name], a seasoned frontend developer with a passion for crafting exceptional digital experiences. With a robust background spanning over a decade in the dynamic realm of frontend development, I have had the privilege of contributing my skills to numerous companies across the globe.
